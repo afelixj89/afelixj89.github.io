726eeeb615b06e2afbdf5aa6ca1b3a3f1cbeb6a0
Antonio Felix <afelixj89@gmail.com>
2024-02-06 12:25:16 -0500

    Added CSS

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 
 const searchButton = document.querySelector(".submitButton");
 const seachBarResults = document.querySelector("#pokName");
+const showImage = document.querySelector(".pokemonPicture")
+const height = document.querySelector(".height")
+const weight = document.querySelector(".weight")
+const id = document.querySelector(".id")
+const abilities = document.querySelector(".abilities")
+const pokemonName = document.querySelector(".pokemonName")
+console.log(".pokemonName")
+
 
 
 
@@ -11,17 +19,32 @@ searchButton.addEventListener("click", function(e) {
     const searchBarValue = seachBarResults.value;
     const searchBarLower= searchBarValue.toLowerCase()
     const url = `https://pokeapi.co/api/v2/pokemon/${searchBarLower}`;
-    console.log(searchBarLower)
+    // console.log(searchBarLower)
 
     
         fetch(url)
             .then((res) =>{ 
-              // console.log(res)
+              
                 return res.json();
     })
                     .then((data) => {
                         pokemon = data.results //results is the array that pulls the pokemon out from the res iterable
-                        console.log(data)
+                        // console.log(data.sprites.front_default);
+
+                        const imageLink = data.sprites.front_default
+                        showImage.setAttribute("src", imageLink)
+                        console.log(data.game_indices[0].version.name)
+                        pokemonName.innerHTML = `Name: ${data.name}`
+                        height.innerHTML = `Height: ${data.height} `
+                        weight.innerHTML = `Weight: ${data.weight}`
+                        id.innerHTML = `Pokemon ID: ${data.id}`
+                        
+                        
+                        // id.innerHTML = `Abilities: ${data.a}`
+
+
+                        
+
                       });
 
     });
